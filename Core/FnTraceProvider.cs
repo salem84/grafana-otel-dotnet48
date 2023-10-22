b39b483851ba8783f518735e9eb4b6f59b111e99
salem84 <salem84@users.noreply.github.com>
2023-10-22 20:42:51 +0200

    tracing enabled and refactoring

---
+++ Core/FnTraceProvider.cs
@@ -0,0 +1,42 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using System;
+
+namespace Core
+{
+    public class FnTraceProvider
+    {
+        private static TracerProvider _tracerProvider;
+
+        public static void Initialize()
+        {
+            if (_tracerProvider != null)
+                throw new InvalidOperationException("FnTraceProvider already initialized!");
+
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+            //.AddAspNetInstrumentation()
+
+            // Other configuration, like adding an exporter and setting resources
+            .AddSource("*")
+            .AddConsoleExporter()
+            .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://localhost:4318/v1/traces");
+                opt.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+            })
+            //.SetResourceBuilder(
+            //    ResourceBuilder.CreateDefault()
+            //        .AddService(serviceName: "my-service-name", serviceVersion: "1.0.0"))
+
+            .Build();
+        }
+
+        public static void Dispose()
+        {
+            if (_tracerProvider != null)
+            {
+                _tracerProvider.Dispose();
+            }
+        }
+    }
+}
